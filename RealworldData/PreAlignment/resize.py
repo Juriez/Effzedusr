84a07fdf9541e3e68b78d9a473977ff2dc474e31
Mahir Faisal <113287415+Juriez@users.noreply.github.com>
2026-06-13 22:27:44 +0600

    Update resize.py
    Updated source and destination directory paths to be relative using os.path.join for better portability.

--- RealworldData/PreAlignment/resize.py
+++ RealworldData/PreAlignment/resize.py
@@ -2,10 +2,15 @@ import os
 from PIL import Image,ImageOps
 
 # Set source and destination folders
-source_dir_tele = "D:\ZeDuSR\zedusr\RealworldData\Data\HighRes\TeleView"
-dest_dir_tele = "D:\ZeDuSR\zedusr\RealworldData\Data\TeleView"
-source_dir_wide = "D:\ZeDuSR\zedusr\RealworldData\Data\HighRes\WideView"
-dest_dir_wide = "D:\ZeDuSR\zedusr\RealworldData\Data\WideView"
+# source_dir_tele = "D:\ZeDuSR\zedusr\RealworldData\Data\HighRes\TeleView"
+# dest_dir_tele = "D:\ZeDuSR\zedusr\RealworldData\Data\TeleView"
+# source_dir_wide = "D:\ZeDuSR\zedusr\RealworldData\Data\HighRes\WideView"
+# dest_dir_wide = "D:\ZeDuSR\zedusr\RealworldData\Data\WideView"
+
+source_dir_tele = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Data", "HighRes", "TeleView")
+dest_dir_tele   = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Data", "TeleView")
+source_dir_wide = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Data", "HighRes", "WideView")
+dest_dir_wide   = os.path.join(os.path.dirname(os.path.dirname(__file__)), "Data", "WideView")
 
 # Create destination directory if it doesn't exist
 os.makedirs(dest_dir_tele, exist_ok=True)
