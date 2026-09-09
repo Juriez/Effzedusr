e4de4878eea3f1a4e1f8b65e902982f89398023e
Mahir Faisal <113287415+Juriez@users.noreply.github.com>
2026-09-09 14:40:03 +0600

    Enhance Saver class with fallback for TensorBoard
    Added a fallback writer for TensorBoard logging when the required libraries are not installed. Updated the create_summary method to use the new summary writer function.

--- Alignment/utils/saver.py
+++ Alignment/utils/saver.py
@@ -2,7 +2,45 @@ import os
 import torch
 from collections import OrderedDict
 import glob
-from tensorboardX import SummaryWriter
+
+
+class _NoOpWriter(object):
+    """Fallback used when no TensorBoard backend is installed.
+
+    Swallows all logging calls so training can run without tensorboardX /
+    torch.utils.tensorboard being available.
+    """
+
+    def add_scalar(self, *args, **kwargs):
+        pass
+
+    def add_scalars(self, *args, **kwargs):
+        pass
+
+    def add_image(self, *args, **kwargs):
+        pass
+
+    def add_histogram(self, *args, **kwargs):
+        pass
+
+    def flush(self):
+        pass
+
+    def close(self):
+        pass
+
+
+def _make_summary_writer(log_dir):
+    try:
+        from tensorboardX import SummaryWriter
+    except ImportError:
+        try:
+            from torch.utils.tensorboard import SummaryWriter
+        except ImportError:
+            print("[WARNING] tensorboardX not installed; TensorBoard logging disabled.")
+            return _NoOpWriter()
+    return SummaryWriter(log_dir=log_dir)
+
 
 class Saver(object):
 
@@ -27,7 +65,43 @@ class Saver(object):
             print(out, file=f)
 
     def create_summary(self):
-        writer = SummaryWriter(log_dir=os.path.join(self.experiment_dir))
+        writer = _make_summary_writer(log_dir=os.path.join(self.experiment_dir))
         return writer
+
+
+
+
+
+# import os
+# import torch
+# from collections import OrderedDict
+# import glob
+# from tensorboardX import SummaryWriter
+
+# class Saver(object):
+
+#     def __init__(self, args):
+#         self.args = args
+#         self.directory = os.path.join(args.output_path, args.dataset)
+#         self.runs = sorted(glob.glob(os.path.join(self.directory, 'experiment_*')))
+#         run_id = int(self.runs[-1].split('_')[-1]) + 1 if self.runs else 0
+
+#         self.experiment_dir = os.path.join(self.directory, 'experiment_{}'.format(str(run_id)))
+#         if not os.path.exists(self.experiment_dir):
+#             os.makedirs(self.experiment_dir)
+
+#     def save_checkpoint(self, state, filename='checkpoint.pth.tar'):
+#         """Saves checkpoint to disk"""
+#         filename = os.path.join(self.experiment_dir, filename)
+#         torch.save(state, filename)
+
+#     def print_log(self,out):
+#         print(out)
+#         with open(os.path.join(self.experiment_dir , 'trainout.txt'), 'a+') as f:
+#             print(out, file=f)
+
+#     def create_summary(self):
+#         writer = SummaryWriter(log_dir=os.path.join(self.experiment_dir))
+#         return writer
 
 
